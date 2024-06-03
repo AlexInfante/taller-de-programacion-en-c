cccf69d039e059b86183b7766972f4fd6b6211df
AlexInfante <danielinfante844@gmail.com>
2024-06-03 01:37:59 -0500

    Add files via upload

---
+++ factorial/while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int factorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+    int fact = 1;
+    int i = 2;
+    while (i <= num) {
+        fact *= i;
+        i++;
+    }
+    return fact;
+}
+int main (){
+    int numero;
+    printf ("Ingrese un número: ");
+    scanf ("%d", &numero);
+
+    printf ("El factorial de %d es: %d\n", numero, factorial(numero));
+
+return 0;
+}
