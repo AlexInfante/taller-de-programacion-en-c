5826112bb8ea91a2b4783a80bdaa6b433c96202b
AlexInfante <danielinfante844@gmail.com>
2024-06-08 10:04:09 -0500

    do_while.c

--- MCD/do_while.c
+++ MCD/do_while.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-int mcd(int a, int b) {
-    int mcd = 1;
-    int i = 1;
-    
-    do {
-        if (a % i == 0 && b % i == 0) {
-            mcd = i;
-            
-        }
-        
-        i++;
-        
-    } while (i<= a && i <= b);
-    
-    return mcd;
-}
-
 int main (){
     int num1, num2;
     printf("Ingrese el primer numero: ");
     scanf("%d",&num1);
     printf("Ingrese el segundo numero: ");
     scanf("%d",&num2);
+       int mcd = 1;
+    int i = 1;
     
-    printf("El MCD de %d y %d es: %d\n", num1, num2, mcd(num1, num2));
+    do {
+        if (num1 % i == 0 && num2 % i == 0) {
+            mcd = i;
+            
+        }
+        
+        i++;
+        
+    } while (i<= num1 && i <= num2);
+    
+    printf("El MCD de %d y %d es: %d\n", num1, num2, mcd);
     return 0;
 }
